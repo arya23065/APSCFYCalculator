efc4710a91556382b82ba780546d1b9decd108f3
arya23065 <arya.subramanyam@alumni.ubc.ca>
2020-05-12 15:59:13 +0530

    Equation parsing for calculator need to be done

--- page1.js
+++ page1.js
@@ -15,6 +15,47 @@ function calculateCP(){
         document.getElementById("cp1").value = a2*b3 - a3*b2; 
         document.getElementById("cp2").value = - (a1*b3 - a3*b1); 
         document.getElementById("cp3").value = a1*b2 - a2*b1; 
-
     }
 }
+
+equation = "";
+
+function backspaceCalc(Calculator) {   
+    if (equation != ""){
+        equation = Calculator.calcDisplay.value.substring(0, Calculator.calcDisplay.value.length-1);
+        Calculator.calcDisplay.value = equation;
+    }
+}
+
+function clearCalc(Calculator) {
+    equation = "";
+    Calculator.calcDisplay.value = equation;
+}
+
+function calcClick(entry, Calculator) {
+    if (entry == "cos(" || entry == "sin("|| entry == "tan(") {
+        clearCalc(Calculator);
+    }
+    equation = equation.concat(String(entry));
+    Calculator.calcDisplay.value = equation;
+    if(entry == "%") evaluate(Calculator);
+}
+          
+function evaluateCalc(Calculator) {               
+    if (Calculator.calcDisplay.value.includes("!")) {           
+        var entryLength = Calculator.calcDisplay.value.length; 
+        n = Number(Calculator.calcDisplay.value.substring(0, entryLength-1)); 
+        f = 1; 
+                  
+        for(i = 2; i <= n; i++) 
+            f = f*i; 
+
+        Calculator.calcDisplay.value = f; 
+
+    } else if(Calculator.calcDisplay.value.includes("%")) { 
+        var entryLength = Calculator.calcDisplay.value.length; 
+        n = Number(Calculator.calcDisplay.value.substring(0, entryLength-1)); 
+        Calculator.calcDisplay.value = n/100; 
+    } else     
+    Calculator.calcDisplay.value = eval(Calculator.calcDisplay.value); 
+} 
